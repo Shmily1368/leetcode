9a80574de229d08b3963eff2c3ad32154e9c4b62
Shmily1368 <1099509685@qq.com>
2021-06-16 13:37:31 +0800

    second day T2

--- sequence/2.两数相加.py
+++ sequence/2.两数相加.py
@@ -11,6 +11,37 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+    def addTwoNumbers(self, l1, l2):
+        re = ListNode(0)
+        r = re
+        s = 0
+        while(l1 or l2):
+            
+            if l1:
+                x = l1.val 
+            else:
+                x = 0
+            if l1!= None:
+                l1 = l1.next
+            
+            if l2:
+                y = l2.val
+            else:
+                y = 0
+            if l2!=None:
+                l2 = l2.next
+            tem = x+y+s
+            s = tem//10
+            r.next = ListNode(tem%10)
+            r = r.next
+        if s>0:
+            r.next = ListNode(1)
+        return re.next
+
+
+
+        
+        
+
 # @lc code=end
 
