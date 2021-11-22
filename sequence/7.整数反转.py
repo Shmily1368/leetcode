0ce615c34c2b275785e7d6fe78212b1947df666b
Shmily1368 <1099509685@qq.com>
2021-11-22 20:22:07 +0800

    2021/11/22 version 2

--- sequence/7.整数反转.py
+++ sequence/7.整数反转.py
@@ -7,6 +7,20 @@
 # @lc code=start
 class Solution:
     def reverse(self, x: int) -> int:
-        return True
+        INT_MIN, INT_MAX = -2**31, 2**31 - 1
+        n=0
+        while x != 0:
+            if n < INT_MIN //10 + 1 or n > INT_MAX//10:
+                return 0
+            yu = x%10
+            if x < 0 and yu >0:
+                yu -= 10
+            x = (x - yu)//10
+            n = n*10 + yu
+        return  n
+        # while x!=0:
+        #     n = n*10 + x%10
+        #     x = int(x/10)
+        # return n if int(n) == n else 0
 # @lc code=end
 
